cc8f8f93ba9eb89676870ec3801d64dd36eb7aea
Imran Hameed <idhameed@protonmail.com>
2024-01-30 18:20:19 -0800

    Use a stub implementation of musl global state when musl is embedded in a larger glibc program.
    This commit hides all pthread internal layout information when musl is
    being compiled as a layer atop glibc. It also changes the shape and
    field names of some musl-internal global variables. This is done to more
    easily catch unexpected dependencies on libc internal state.
    
    Also, remove a stray reference to __aio_close from close(). It shows up
    in the test output binaries and while it's benign, I don't like that
    it's there.
    
    FIXME: Currently, accesses to musl-internal locale data will trap; this
    should be reevaluated and possibly layered on top of glibc.

--- src/unistd/close.c
+++ src/unistd/close.c
@@ -3,16 +3,20 @@
 #include "aio_impl.h"
 #include "syscall.h"
 
+#if GMUSL_gcompat__musl_global_state
 static int dummy(int fd)
 {
 	return fd;
 }
 
 weak_alias(dummy, __aio_close);
+#endif
 
 int close(int fd)
 {
+#if GMUSL_gcompat__musl_global_state
 	fd = __aio_close(fd);
+#endif
 	int r = __syscall_cp(SYS_close, fd);
 	if (r == -EINTR) r = 0;
 	return __syscall_ret(r);
